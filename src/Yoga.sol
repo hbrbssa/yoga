bd1af9e80c33147852c526f8b4b876bb6eadabda
Duncan Townsend <git@duncancmt.com>
2025-11-22 13:30:26 +0100

    `unlockCallback`

--- src/Yoga.sol
+++ src/Yoga.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {IERC20} from "@forge-std/interfaces/IERC20.sol";
 import {IERC165} from "@forge-std/interfaces/IERC165.sol";
+
 import {ERC721} from "@solady/tokens/ERC721.sol";
+import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
+import {ReentrancyGuardTransient} from "@solady/utils/ReentrancyGuardTransient.sol";
 
 import {BalanceDelta} from "@uniswapv4/types/BalanceDelta.sol";
 import {PoolKey} from "@uniswapv4/types/PoolKey.sol";
@@ -12,19 +16,72 @@ import {IUnlockCallback} from "@uniswapv4/interfaces/IUnlockCallback.sol";
 
 //import {MultiCallContext} from "lib/MultiCallContext.sol";
 
-contract Yoga is IERC165, IUnlockCallback, ERC721 /*, MultiCallContext */ {
+struct SimpleModifyLiquidityParams {
+    // the lower and upper tick of the position
+    int24 tickLower;
+    int24 tickUpper;
+    // how to modify the liquidity
+    int256 liquidityDelta;
+}
+
+library CurrencySafeTransferLib {
+    using SafeTransferLib for address;
+
+    function safeTransferFrom(Currency token, address from, address to, uint256 amount) internal {
+        return Currency.unwrap(token).safeTransferFrom(from, to, amount);
+    }
+}
+
+contract Yoga is IERC165, IUnlockCallback, ERC721 /*, MultiCallContext */, ReentrancyGuardTransient {
+    using CurrencySafeTransferLib for Currency;
+
     IPoolManager public constant POOL_MANAGER = IPoolManager(0x1f98400000000000000000000000000000000004);
 
     uint256 public nextTokenid = 1;
 
-    function mint(PoolKey calldata key, ModifyLiquidityParams calldata params) external returns (uint256 tokenId) {
+    function mint(PoolKey calldata key, SimpleModifyLiquidityParams calldata params) external nonReentrant returns (uint256 tokenId) {
         unchecked {
             tokenId = nextTokenId++;
         }
         _safeMint(msg.sender, tokenId);
     }
 
+    function _settle(address owner, address payable recipient, Currency currency, int128 amount) private {
+        if (amount < 0) {
+            uint256 debt;
+            unchecked {
+                debt = -int256(amount);
+            }
+            if (currency.isAddressZero()) {
+                POOL_MANAGER.settle{value: debt}();
+            } else {
+                POOL_MANAGER.sync(currency);
+                currency.safeTransferFrom(owner, address(POOL_MANAGER), debt);
+                POOL_MANAGER.settle();
+            }
+        } else {
+            uint256 credit = uint256(int256(amount));
+            POOL_MANAGER.take(currency, recipient, credit);
+        }
+    }
+
     function unlockCallback(bytes calldata data) external returns (bytes memory) {
         require(msg.sender == address(POOL_MANAGER));
+        (address owner, address payable recipient, PoolKey memory key, bytes32 salt, SimpleModifyLiquidityParams[] memory params) = abi.decode(data, (address, address payable, PoolKey, bytes32, SimpleModifyLiquidityParams[]));
+
+        BalanceDelta delta;
+        ModifyLiquidityParams memory managerParams;
+        managerParams.salt = salt;
+        for (uint256 i; i < params.length; i++) {
+            SimpleModifyLiquidityParams memory simpleParams = params[i];
+            managerParams.tickLower = simpleParams.tickLower;
+            managerParams.tickUpper = simpleParams.tickUpper;
+            managerParams.liquidityDelta = simpleParams.liquidityDelta;
+            (BalanceDelta callerDelta, ) = POOL_MANAGER.modifyLiquidity(key, managerParams, ""); // TODO: hookData
+            delta += callerDelta;
+        }
+
+        _settle(owner, recipient, key.currency0, delta.amount0());
+        _settle(owner, recipient, key.currency1, delta.amount1());
     }
 }
